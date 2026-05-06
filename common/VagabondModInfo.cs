351582a9a1ba1c72009737050082136acc64e7ac
Mark <mark.eliasen@pm.me>
2026-05-06 23:09:33 +0100

    0.7.0

--- common/VagabondModInfo.cs
+++ common/VagabondModInfo.cs
@@ -4,7 +4,7 @@ public static class VagabondModInfo
 {
     public const string Guid = "dev.oogabooga.spt-vagabond";
     public const string Name = "Vagabond";
-    public const string Version = "0.6.1";
+    public const string Version = "0.7.0";
     public const string Author = "Oogabooga.dev";
     public const string Url = "https://github.com/MrEliasen/spt-vagabond";
     public const string License = "MIT";
